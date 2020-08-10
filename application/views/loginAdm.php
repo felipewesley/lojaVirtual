9309aa43cf56079dc6f66280cd61a8fe561b8e5b
Felipe Wesley <48454653+felipewesley@users.noreply.github.com>
2020-08-10 19:07:57 +0200

    Bloqueio seleção conteúdo com evento js

--- application/views/loginAdm.php
+++ application/views/loginAdm.php
@@ -9,7 +9,7 @@
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="../design/styles/login_adm_style.css">
 </head>
-<body>
+<body onselectstart="return false">
 
     <div class="container-main-box">
         <div class="main-box">
